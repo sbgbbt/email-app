294e4300476726313f3b51394ad4f5f391b156a4
陈杨嘉 <chenyangjia@nichols-ve501-0252.apn.wlan.upenn.edu>
2018-10-16 23:21:07 -0400

    Users can see survey results

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 
 class App extends Component {
@@ -18,16 +18,14 @@ class App extends Component {
 
 	render() {
 		return(
-			<div className="container">
 				<BrowserRouter>
-					<div>
+					<div className="container">
 						<Header />
 						<Route exact path="/" component={Landing} />
 						<Route exact path="/surveys" component={Dashboard} />
 						<Route exact path="/surveys/new" component={SurveyNew} />
 					</div>
 				</BrowserRouter>
-			</div>
 		);
 	}
 };
